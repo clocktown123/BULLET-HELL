75f6b2c06a0449ebdca6d0018ff985b08cb4257e
clocktown123 <146383205+clocktown123@users.noreply.github.com>
2024-03-12 15:40:18 -0600

    Create missiles.cpp

---
+++ missiles.cpp
@@ -0,0 +1,45 @@
+#include "missiles.h"
+#include "main.cpp"
+
+missiles::missiles() {
+	xpos = 400;
+	ypos = 700;
+	
+}
+
+void missiles::move() {
+
+}
+
+void missiles::draw() {
+
+}
+
+void missiles::kill() {
+
+}
+
+bool missiles::offScreen() {
+
+}
+
+bool missiles::isAlive() {
+
+}
+
+int missiles::getx() {
+
+}
+
+int missiles::gety() {
+
+}
+
+void missiles::shoot(int xpos, int ypos) {
+
+
+}
+
+bool missiles::collide(double xpos, double ypos) {
+
+}
